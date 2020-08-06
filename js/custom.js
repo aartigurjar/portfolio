a2b4fef67abcc5e201c27b69ab9c70dc63c74f22
aartigurjar <36697551+aartigurjar@users.noreply.github.com>
2020-08-06 13:29:23 +1000

    first commit

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+
+    $(document).on('click','.navbar-collapse.in',function(e) {
+        if( $(e.target).is('a') ) {
+            $(this).collapse('hide');
+        }
+    });
+
+
+    $(function() {
+        $('.navbar-default a').bind('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top - 49
+            }, 1000);
+            event.preventDefault();
+        });
+    });
